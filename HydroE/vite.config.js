544ffe2ede2592e3e83fe63fe67f9b6686c7c168
MAGNUS. W. HINGE <122803385+MacBeef@users.noreply.github.com>
2024-11-08 12:22:13 +0100

    Build project for deployment

--- HydroE/vite.config.js
+++ HydroE/vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? `/${process.env.VITE_APP_REPO_NAME}/`
-      : "/",
-  envPrefix: "VITE_",
+  base: process.env.NODE_ENV === "production" ? "/Hydro/" : "/",
 });
